237c4ed60e6f73270e10225fa2771db7b8894a2f
mikohei <mikohei@amazon.co.jp>
2021-01-15 08:51:28 +0900

    [ADD] Calc Pg

--- ConsoleApp3/Program.fs
+++ ConsoleApp3/Program.fs
@@ -1,13 +1,8 @@
-﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+﻿module HelloSquare
 
-open System
-
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+let square x = x * x
 
 [<EntryPoint>]
 let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
-    0 // return an integer exit code
+    printfn "%d squared is: %d!" 12 (square 12)
+    0 // Return an integer exit code
---
+++ calculation/Program.fs
@@ -0,0 +1,27 @@
+﻿// Boolean
+printfn "%b" (4 = 2 * 2)
+printfn "%b" (true && true) // and
+printfn "%b" (true && false)
+printfn "%b" (false || false) // or
+printfn "%b" (false || true)
+printfn "%b" (5 <> 4)
+printfn "%b" (5 = 4)
+printfn "%b" (not (5 <> 4)) // Enclose the expression in parentheses
+
+// Calc
+printfn "%i" (5 * 3)
+
+// string
+let s = "hogehoge"
+printfn "%s" s
+
+// Calc
+let x = 2.4
+let y = 3.0
+let z = 16
+let u = 7
+printfn "%f" (x + y) // float
+printfn "%f" (x - y)
+printfn "%f" (x * y)
+printfn "%f" (x / y)
+printfn "%i" (z % u)
