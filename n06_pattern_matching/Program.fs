1ec0e25f9e13944780a42da07b89a3f6d789ac4f
mikohei <mikohei@amazon.co.jp>
2021-01-16 23:42:01 +0900

    [ADD] Pattern matching

---
+++ n06_pattern_matching/Program.fs
@@ -0,0 +1,25 @@
+﻿// --- pattern matching ---
+// match expr with
+// | pat-1 -> result-1
+// | pat-2 -> result-2
+// | pat-3 when expr -> result-3
+// | ...
+// | _ -> defaultResult
+
+let rec fact x =
+    match x with
+    | 0
+    | 1 -> 1
+    | x -> x * (fact (x - 1))
+
+
+let res = fact 5
+printfn "%d" res
+
+let mydiv (x: float) (y: float) =
+    match y with
+    | 0.0 -> None
+    | _ -> Some(x / y) // Option type
+
+printfn "%A" (mydiv 2.0 3.0)
+printfn "%A" (mydiv 3.0 0.0)
