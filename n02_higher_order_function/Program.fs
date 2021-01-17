dda7f367115c7e21a73d7e37a12b462c68e1b871
mikohei <mikohei@amazon.co.jp>
2021-01-17 13:30:23 +0900

    [ADD] higher-order-function

---
+++ n02_higher_order_function/Program.fs
@@ -0,0 +1,20 @@
+﻿// --- higher-order function ---
+//  --- map ---
+// map f x
+
+let Lsquare (lnum: int list) =
+    let square = (fun x -> x * x)
+    let lsnum = List.map square lnum
+    printfn "num^2 = %A" lsnum
+
+
+let num = [ 1 .. 10 ]
+Lsquare num
+
+// --- fold ---
+
+let fold_sum (num: int list) =
+    let sum = List.fold (fun x y -> x + y) 0 num
+    printfn "sum = %d" sum
+
+fold_sum [ 1 .. 10 ]
