af53c2471d68b2465968c9cd876436a83a47ba53
mikohei <mikohei@amazon.co.jp>
2021-01-17 14:50:42 +0900

    [ADD] interface

--- n03_inheritance/Program.fs
+++ n03_inheritance/Program.fs
@@ -1,62 +1,34 @@
-﻿type Rect =
-    class
-        val height: float
-        val width: float
-        new(h, w) = { height = h; width = w }
-        member x.Area = x.height * x.width
-    end
-
-type Square =
-    class
-        inherit Rect
-        new(h, w) = { inherit Rect(h, w) }
-    end
-
-let RectObj (x: float) (y: float) =
-    let rect = Rect(x, y)
-    printfn "Rect object rect:"
-    printfn "Height = %f" x
-    printfn "width = %f" y
-    printfn "area = %f" rect.Area
-
-let SqObj (x: float) =
-    let sq = Square(x, x)
-    printfn "Square object sq:"
-    printfn "height = width = %f" x
-    printfn "area = %f" sq.Area
-
-
-RectObj 3.0 4.0
-SqObj 5.0
-
-type Rect2 =
-    class
-        val height: float
-        val width: float
-
-        new(h, w) as this =
-            { height = h; width = w }
-            then
-                printfn "height = %f" this.height
-                printfn "width = %f" this.width
-                printfn "area = %f" this.Area
-
-        member this.Area = this.height * this.width
-    end
-
-type Square2 =
-    class
-        inherit Rect2
-        new(h, w) = { inherit Rect2(h, w) }
-    end
-
-let RectObj2 (x: float) (y: float) =
-    printfn "Rect object rect:"
-    Rect2(x, y)
-
-let SqObj2 (x: float) =
-    printfn "Square object sq:"
-    Square2(x, x)
-
-RectObj2 3.0 4.0
-SqObj2 5.0
+﻿type IArea =
+ interface
+ abstract Area : unit -> float
+end
+type Rect = class
+  val height : float
+  val width : float
+  new (h,w) =
+       {height = h; width = w;}
+  interface IArea with 
+    member x.Area() = 
+      x.height * x.width
+end
+type Circle = class
+  val radius : float
+  new (r) = 
+       {radius = r;}
+  interface IArea with 
+    member x.Area() = 
+      x.radius * x.radius * 3.141592
+end;;
+let RectObj (x : float) (y : float) = 
+   let rect = new Rect(x, y)
+   let ra = (rect :> IArea).Area()
+   printfn "Rect object rect:"
+   printfn "height = %f" x
+   printfn "width = %f" y
+   printfn "area = %f" ra;;
+let CircObj (x : float)  = 
+   let circ = new Circle(x)
+   let ca = (circ :> IArea).Area()
+   printfn "Circle object circ:"
+   printfn "radius = %f" x
+   printfn "area = %f" ca;;
--- n05-interface/Program.fs
+++ n05-interface/Program.fs
@@ -1,30 +1,30 @@
-﻿[<AbstractClass>]
-type Shape =
-    class
-        new() = {  }
+﻿type IArea =
+    interface
         abstract Area: unit -> float
     end
 
 type Rect =
     class
-        inherit Shape
         val height: float
         val width: float
         new(h, w) = { height = h; width = w }
-        override x.Area() = x.height * x.width
+
+        interface IArea with
+            member x.Area() = x.height * x.width
     end
 
 type Circle =
     class
-        inherit Shape
         val radius: float
         new(r) = { radius = r }
-        override x.Area() = x.radius * x.radius * 3.141592
+
+        interface IArea with
+            member x.Area() = x.radius * x.radius * 3.141592
     end
 
 let RectObj (x: float) (y: float) =
     let rect = Rect(x, y)
-    let ra = rect.Area()
+    let ra = (rect :> IArea).Area()
     printfn "Rect object rect:"
     printfn "height = %f" x
     printfn "width = %f" y
@@ -32,11 +32,10 @@ let RectObj (x: float) (y: float) =
 
 let CircObj (x: float) =
     let circ = Circle(x)
-    let ca = circ.Area()
+    let ca = (circ :> IArea).Area()
     printfn "Circle object circ:"
     printfn "radius = %f" x
     printfn "area = %f" ca
 
-
-RectObj 3.0 1.5
+RectObj 6.0 7.0
 CircObj 1.0
