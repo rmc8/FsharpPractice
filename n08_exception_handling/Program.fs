642bfef9129c64fe599747f681dbb34b90085d72
mikohei <mikohei@amazon.co.jp>
2021-01-17 13:04:55 +0900

    [ADD] exception handling

---
+++ n08_exception_handling/Program.fs
@@ -0,0 +1,33 @@
+﻿// -- exception handling ---
+// * System.ArithmeticException
+// * System.ArrayTypeMismatchException
+// * System.DivideByZeroException
+// * System.OverFlowException
+// * System.IndexOutOfRangeException
+// --- try-catch ---
+// try expr with rules
+// --- Pattern rule ---
+// :? System.FormatException -> value
+
+let Input msg =
+    printf msg
+
+    try
+        float (System.Console.ReadLine())
+    with :? System.FormatException -> System.Double.NaN
+
+
+let x = Input("x = ")
+let y = Input("y = ")
+printfn "%f + %f = %f" x y (x + y)
+
+
+let mydiv (x: float) (y: float) =
+    try
+        x / y
+    finally
+        printfn "DivideByZero Exception if infinity"
+
+
+mydiv 3.0 2.0
+mydiv 3.0 0.0
