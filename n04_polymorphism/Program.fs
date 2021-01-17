e381f6310ae1c5615475c03db4b405e1c2bb8b67
mikohei <mikohei@amazon.co.jp>
2021-01-17 14:43:44 +0900

    [ADD] polymorphism

---
+++ n04_polymorphism/Program.fs
@@ -0,0 +1,43 @@
+﻿// FS39.fs: ポリモフィズム
+[<AbstractClass>]
+type Shape =
+    class
+        new() = {  }
+        abstract Area: unit -> float
+    end
+
+type Rect =
+    class
+        inherit Shape
+        val height: float
+        val width: float
+        new(h, w) = { height = h; width = w }
+        override x.Area() = x.height * x.width
+    end
+
+type Circle =
+    class
+        inherit Shape
+        val radius: float
+        new(r) = { radius = r }
+        override x.Area() = x.radius * x.radius * 3.141592
+    end
+
+let RectObj (x: float) (y: float) =
+    let rect = Rect(x, y)
+    let ra = rect.Area()
+    printfn "Rect object rect:"
+    printfn "height = %f" x
+    printfn "width = %f" y
+    printfn "area = %f" ra
+
+let CircObj (x: float) =
+    let circ = Circle(x)
+    let ca = circ.Area()
+    printfn "Circle object circ:"
+    printfn "radius = %f" x
+    printfn "area = %f" ca
+
+
+RectObj 3.0 1.5
+CircObj 1.0
