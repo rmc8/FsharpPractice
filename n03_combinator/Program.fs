1430168b889554e9888cf8ec4f7a80f23c3bbcf8
mikohei <mikohei@amazon.co.jp>
2021-01-17 13:40:10 +0900

    [ADD] combinator

---
+++ n03_combinator/Program.fs
@@ -0,0 +1,24 @@
+﻿// --- conbinator ---
+let IF x y z = x y z
+let TRUE x y = x
+let FALSE x y = y
+let K x y = x
+let I x = x
+
+let zero_test =
+    function
+    | 0 -> TRUE
+    | otherwise -> FALSE
+
+printfn "%A" (IF (zero_test 0) "x is 0" "x is not zero")
+printfn "%A" (IF (zero_test 50) "x is 0" "x is not zero")
+
+// --- fixpoint combinator ---
+
+let Yfact n =
+    let rec Y F x = F (Y F) x
+    Y (fun fact x -> if x = 0 then 1 else x * fact (x - 1)) n
+
+printfn "%A" (Yfact 0)
+printfn "%A" (Yfact 1)
+printfn "%A" (Yfact 5)
